1183a23846833b1aa4f020ddd28c21b1af3e2f3e
Igor Suleymanov <radiohead@users.noreply.github.com>
2020-12-23 09:24:07 +0200

    Implement identity service client and tests (#1)

---
+++ client/src/main/kotlin/com/speechly/client/identity/IdentityService.kt
@@ -0,0 +1,48 @@
+package com.speechly.client.identity
+
+import com.speechly.api.identity.v1.*
+import java.io.Closeable
+import java.util.UUID
+
+/**
+ * This class represents a service that returns authentication tokens obtained from Speechly Identity API.
+ * By default it uses a client that uses Speechly Identity gRPC API.
+ *
+ * @param client - an implementation of identity client logic.
+ */
+class IdentityService(private val client: IdentityClient) : Closeable by client {
+    companion object {
+        /**
+         * Creates a new identity service using default gRPC client implementation.
+         *
+         * @param target - the address of the API endpoint to connect to, e.g. "api.speechly.com"
+         * @param secure - whether to use secured (TLS) or plaintext connection
+         */
+        fun forTarget(
+            target: String = "api.speechly.com",
+            secure: Boolean = true
+        ): IdentityService {
+            return IdentityService(
+                GrpcIdentityClient.forTarget(target, secure)
+            )
+        }
+    }
+
+    /**
+     * Fetches a new authentication token and decodes it into `AuthToken`.
+     *
+     * @param appId - Speechly application ID to use for authentication.
+     * @param deviceId - Speechly device ID to use for authentication.
+     */
+    suspend fun authenticate(appId: UUID, deviceId: UUID): AuthToken {
+        val request = IdentityOuterClass.LoginRequest
+            .newBuilder()
+            .setAppId(appId.toString())
+            .setDeviceId(deviceId.toString())
+            .build()
+
+        val response = client.login(request)
+
+        return AuthToken.fromJWT(response.token)
+    }
+}
